4172550817774d8f3e08b9eb78b4b8eb6fa5561f
Terrence <owusuterrence@gmail.com>
2017-09-19 10:13:23 +0100

    Added contact box

--- app.py
+++ app.py
@@ -1,21 +1,50 @@
+class ContactBox:
+    def __init__(self, contacts=[]):
+        self.contacts = contacts
+
+    def add_contact(self, contact):
+        self.contacts.append(contact)
+        print("Contact added successfully")
+
+    def search_contact(self, name=''):
+        name = input('Enter name to search: ')
+        for contact in self.contacts:
+            if contact.name == name:
+                return print(contact.name, contact.age, contact.number, contact.gender)
+            else:
+                return print('Contact not found')
+
+    def delete_contact(self, name=''):
+        name = input('Enter name to delete: ')
+        for contact in self.contacts:
+            if contact.name == name:
+                self.contacts.remove(contact)
+                print('Contact removed successfully')
+            else:
+                print('Contact not found')
+
 class Contact:
-    def __init__(self, name="", number="", gender="", email="", postal_address=""):
-        self.name = name,
-        self.number = number,
-        self.gender = gender,
-        self.email = email,
-        self.postal_address = postal_address
+    def __init__(self, name='', age='', number='', gender=''):
+        self.name = input('Your name: ')
+        self.number = input('Your number: ')
+        self.age = input('Your age: ')
+        self.gender = input('Your gender: ')
 
 
-def add_contact():
-    name = input("Your name: ")
-    number = input('Your number: ')
-    gender = input('Your gender (M/F>: ')
-    email = input('Your email address: ')
-    postal_address = input('Your postal address: ')
+def main():
+    new_contact = ContactBox()
 
-    contact_new = Contact(name, number, gender, email, postal_address)
-    print('')
-    print("Name: {}\nNumber: {}\nGender: {}\nEmail: {}\nAddress: {}\n".format(contact_new.name, contact_new.number, contact_new.gender, contact_new.email, contact_new.postal_address))
+    while True:
+        ans = int(input('Press 1 to add, 2 to search and 3 to delete: '))
 
-add_contact()
+        if ans == 1:
+            contact = Contact()
+            new_contact.add_contact(contact)
+        elif ans == 2:
+            new_contact.search_contact()
+        elif ans == 3:
+            new_contact.delete_contact()
+        else:
+            print('Sorry bye bye')
+
+main()
